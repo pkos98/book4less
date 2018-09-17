a38931cb241391e0191870fd46f5d48724c3c7b0
Patrick Kostas <pa2kostas@yahoo.de>
2018-09-17 21:42:10 +0200

    Add book_store field

--- lib/book4less/searches/search.ex
+++ lib/book4less/searches/search.ex
@@ -2,6 +2,8 @@ defmodule Book4Less.Searches.Search do
   defstruct [
     :query,
     :result,
-    :search
+    :book_store
   ]
+
+  def put_search(value, key, search), do: Map.update(search, key, value, fn(_x) -> value end)
 end
